342ab0b2427119aaa1ff53e41825210dc6f69e8c
cyprien <cyprientaib1@gmail.com>
2021-03-24 16:50:48 +0100

    Make more request for flights

--- BackEnd/internal/http/controllers/flights.go
+++ BackEnd/internal/http/controllers/flights.go
@@ -12,10 +12,25 @@ import (
 func FligthsBootstrap(app fiber.Router) {
 	app.Get("/", flightsGetlist)
 
+	app.Get("/:city", cityGetFLight)
+
 	app.Patch("/", departuresUpdate)
 
 	app.Delete("/", departuresDelete)
 
+}
+
+func cityGetFLight(c *fiber.Ctx) error {
+
+	filter := "`id` IN SELECT `id_departures` FROM flight(`id_route` IN (SELECT id FROM `route` WHERE `origin` = " + c.Params("city") + " or `arival` = " + c.Params("city") + ")"
+
+	c.JSON(&fiber.Map{
+		"success": true,
+		"value":   sql_request.GetFlight("", filter),
+		"message": "Hello from the other side",
+	})
+
+	return nil
 }
 
 func flightsGetlist(c *fiber.Ctx) error {
